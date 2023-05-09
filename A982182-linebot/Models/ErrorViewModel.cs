0b1bd0b3c47dc22a12096f17608e85481f0ddfbf
A982182 <sherry1598753@gmail.com>
2023-05-09 15:33:33 +0800

    init

---
+++ A982182-linebot/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace A982182_linebot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}

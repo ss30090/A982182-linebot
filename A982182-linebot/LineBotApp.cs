6ad83298b73ce800d35465da365131d2637e8065
ss30090 <sherry1598753@gmail.com>
2023-05-30 16:05:19 +0800

    新增邊緣人員

--- A982182-linebot/LineBotApp.cs
+++ A982182-linebot/LineBotApp.cs
@@ -6,15 +6,18 @@ namespace A982182_linebot;
 public class LineBotApp : WebhookApplication
 {
     private readonly LineMessagingClient _messagingClient;
+ 
+    private static Dictionary<string, string> _pool = new Dictionary<string, string>();
+ 
     public LineBotApp(LineMessagingClient lineMessagingClient)
     {
         _messagingClient = lineMessagingClient;
     }
-
+ 
     protected override async Task OnMessageAsync(MessageEvent ev)
     {
         var result = null as List<ISendMessage>;
-
+ 
         switch (ev.Message)
         {
             //文字訊息
@@ -24,17 +27,103 @@ public class LineBotApp : WebhookApplication
                 var channelId = ev.Source.Id;
                 //使用者Id
                 var userId = ev.Source.UserId;
-                    
+                //使用者輸入的文字
+                var text = ((TextEventMessage)ev.Message).Text;
+ 
+                if (PoolHasMsg(text))
+                {
+                    // 從記憶體池查詢資料
+                    string response = GetResponse(text);
+                    result = new List<ISendMessage>
+                    {
+                        new TextMessage(response)
+                    };
+                }
+                else
+                {
+                    if (CheckFormat(text))
+                    {
+                        //將資料寫入記憶體池
+                        TeachPeople(text);
+                    }
+                }
+ 
+                /*
                 //回傳 hellow
                 result = new List<ISendMessage>
                 {
-                    new TextMessage("嘿~邊緣人~")
+                    new TextMessage("你寂寞嗎?")
                 };
+                */
             }
                 break;
         }
-
+ 
         if (result != null)
             await _messagingClient.ReplyMessageAsync(ev.ReplyToken, result);
+    }
+ 
+    /// <summary>
+    /// 確認是否已經學習過這個對話
+    /// </summary>
+    /// <param name="inputMsg"></param>
+    /// <returns></returns>
+    private bool PoolHasMsg(string inputMsg)
+    {
+        return _pool.ContainsKey(inputMsg);
+    }
+ 
+    /// <summary>
+    /// 用於 已經學習過的對話
+    /// </summary>
+    /// <param name="inputMsg"></param>
+    /// <returns></returns>
+    private string GetResponse(string inputMsg)
+    {
+        return _pool[inputMsg];
+    }
+ 
+    private bool CheckFormat(string inputMsg)
+    {
+        bool result = false;
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            //檢查
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "邊緣人員")
+                {
+                    result = true;
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
+        return result;
+    }
+ 
+    private void TeachPeople(string inputMsg)
+    {
+        try
+        {
+            string[] subs = inputMsg.Split(';');
+            //檢查
+            if (subs.Length == 3)
+            {
+                if (subs[0] == "邊緣人員")
+                {
+                    _pool.Add(subs[1],subs[2]);
+                }
+            }
+        }
+        catch (Exception exp)
+        {
+            Console.WriteLine(exp);
+            throw;
+        }
     }
 }
